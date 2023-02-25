5058f6861cca16b96a95de0f66a7ae487b0cc1c4
Alex Hernandez Duran <hernandezduranluisalejandro4@gmail.com>
2023-02-25 13:27:15 -0600

    Primer commit

---
+++ Session1/r1/src/main/java/enteroBinario.java
@@ -0,0 +1,9 @@
+public class enteroBinario {
+    public static void main(String [] args) {
+        int number = 1540;
+
+        String binary = Integer.toBinaryString(number);
+
+        System.out.println("El número entero " + number + " en binario es: " + binary);
+    }
+}
