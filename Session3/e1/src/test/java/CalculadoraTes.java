a75c59b089899a71149d6f3b479a35718711a650
Alex Hernandez Duran <hernandezduranluisalejandro4@gmail.com>
2023-03-02 19:53:35 -0600

    Se sube Session3

---
+++ Session3/e1/src/test/java/CalculadoraTes.java
@@ -0,0 +1,28 @@
+class CalculadoraTest {
+
+    private Calculadora calculadora = new Calculadora();
+
+    @Test
+    @DisplayName("Prueba suma")
+    void sumaTest() {
+        int esperado = 5;
+
+        assertEquals(esperado, calculadora.suma(3, 2));
+    }
+
+    @Test
+    @DisplayName("Prueba resta")
+    void restaTest() {
+        int esperado = 1;
+
+        assertEquals(esperado, calculadora.resta(3, 2));
+    }
+
+    @Test
+    @DisplayName("Prueba multiplicación")
+    void multiplicaTest() {
+        int esperado = 6;
+
+        assertEquals(esperado, calculadora.multiplica(3, 2));
+    }
+}
