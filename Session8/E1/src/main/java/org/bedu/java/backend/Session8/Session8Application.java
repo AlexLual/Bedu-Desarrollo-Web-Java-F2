f28a0cee544dfac13f8f42279227a4dc71bb6c6c
Alex Hernandez Duran <hernandezduranluisalejandro4@gmail.com>
2023-03-25 15:01:51 -0600

    Se sube Session 8

---
+++ Session8/E1/src/main/java/org/bedu/java/backend/Session8/Session8Application.java
@@ -0,0 +1,13 @@
+package org.bedu.java.backend.Session8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Session8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Session8Application.class, args);
+	}
+
+}
